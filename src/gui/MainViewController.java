35e9998a7c7f9869bff9ced538cca659e2871ef0
Lucileudo Paiva <lucileudodepaivalopes@gmail.com>
2026-07-10 21:09:10 -0300

    Adding database access

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -59,8 +59,8 @@ public class MainViewController implements Initializable {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			VBox newVBox = loader.load();
 
-			Scene maiScene = Main.getMainScene();
-			VBox mainVBox = (VBox) ((ScrollPane) maiScene.getRoot()).getContent();
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
 
 			Node mainMenu = mainVBox.getChildren().get(0);
 			mainVBox.getChildren().clear();
